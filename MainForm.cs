8cf4e1584c17ce6d45a7e765e51d25df476bbae4
kurumpa <kiev.programmer@gmail.com>
2014-10-23 14:41:38 +0300

    Changed to tray-only app

---
+++ MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotSwitcher
+{
+    public class SysTrayApp : Form
+    {
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+        private MenuItem power;
+        private bool running = false;
+
+        public SysTrayApp()
+        {
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+            power = new MenuItem("Turn on", OnPower);
+            trayMenu.MenuItems.Add(power);
+
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "dotSwitcher";
+            trayIcon.Icon = Properties.Resources.icon;
+
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+            trayIcon.ShowBalloonTip(2000, "dotSwitcher", "dotSwitcher started", ToolTipIcon.None);
+
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void OnPower(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                running = false;
+                power.Text = "Turn on";
+            }
+            else
+            {
+                running = true;
+                power.Text = "Turn off";
+            }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                trayIcon.Dispose();
+            }
+            base.Dispose(isDisposing);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace dotSwitcher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SysTrayApp());
         }
     }
 }
