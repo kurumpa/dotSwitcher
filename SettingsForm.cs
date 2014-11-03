8a91b17b2f936fc54bf4984f636fb4e118629ef3
Kirill <kiev.programmer@gmail.com>
2014-11-03 23:31:21 +0200

    removed comments and a strange condition

--- SettingsForm.cs
+++ SettingsForm.cs
@@ -12,9 +12,7 @@ namespace dotSwitcher
 {
     public partial class SettingsForm : Form
     {
-        //http://www.codeproject.com/Articles/442285/Global-Shortcuts-in-WinForms-and-WPF
-        
-        bool keyIsSet; //Would help us to know if the user has set a shortcut.
+        bool keyIsSet;
 
         public SettingsForm()
         {
@@ -29,40 +27,25 @@ namespace dotSwitcher
 
         private void txtButton_KeyDown(object sender, KeyEventArgs e)
         {
-            e.SuppressKeyPress = true;  //Suppress the key from being processed by the underlying control.
-            shortcutTextBox.Text = string.Empty;  //Empty the content of the textbox
-            keyIsSet = false; //At this point the user has not specified a shortcut.
+            e.SuppressKeyPress = true; 
+            shortcutTextBox.Text = string.Empty; 
+            keyIsSet = false; 
 
-            //Set the backspace button to specify that the user does not want to use a shortcut.
             if (e.KeyData == Keys.Back)
             {
                 shortcutTextBox.Text = Keys.None.ToString();
                 return;
             }
 
-            //Make the user specify a modifier. Control, Alt or Shift.
-            //If a modifier is not present then clear the textbox.
-            /*if (e.Modifiers == Keys.None)
+            if (e.Alt || e.Control || e.Shift)
             {
-                MessageBox.Show("You have to specify a modifier like 'Control', 'Alt' or 'Shift'");
-                shortcutTextBox.Text = Keys.None.ToString();
-                return;
-            }*/
-
-            if (e.Modifiers.ToString() != "None")
-            {
-                //A modifier is present. Process each modifier.
-                //Modifiers are separated by a ",". So we'll split them and write each one to the textbox.
                 foreach (string modifier in e.Modifiers.ToString().Split(new Char[] {','}))
                 {
                     shortcutTextBox.Text += modifier + " + ";
                 }
             }
 
-            //KEYCODE contains the last key pressed by the user.
-            //If KEYCODE contains a modifier, then the user has not entered a shortcut. hence, KeyisSet is false
-            //But if not, KeyisSet is true.
-            if (e.KeyCode == Keys.ShiftKey | e.KeyCode == Keys.ControlKey | e.KeyCode == Keys.Menu)
+            if (e.KeyCode == Keys.ShiftKey || e.KeyCode == Keys.ControlKey || e.KeyCode == Keys.Menu)
             {
                 keyIsSet = false;
             }
@@ -72,13 +55,10 @@ namespace dotSwitcher
                 keyIsSet = true;
             }
 
-            //TODO save key code and Modifiers
-
         }
 
         private void txtButton_KeyUp(object sender, KeyEventArgs e)
         {
-            //On KeyUp if KeyisSet is False then clear the textbox.
             if (keyIsSet == false)
             {
                 shortcutTextBox.Text = Keys.None.ToString();
