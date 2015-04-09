d232a3976c073fad166230c7fb5bd36664b23ab7
Georgy <grigoryev@ntpayments.com>
2015-04-10 01:26:33 +0400

    switch keyboard by keys

--- SettingsForm.cs
+++ SettingsForm.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using System.Linq;
 namespace dotSwitcher
 {
     public partial class SettingsForm : Form
     {
-        bool keyIsSet;
-        KeyboardHook kbdHook;
-        TextBox currentTextBox;
-        Settings settings;
-        KeyboardEventArgs currentHotkey;
+        private bool keyIsSet;
+        private KeyboardHook kbdHook;
+        private TextBox currentTextBox;
+        private Settings settings;
+        private KeyboardEventArgs currentHotkey;
 
         public SettingsForm(Settings settings)
         {
             this.settings = settings;
-            
+
             currentTextBox = null;
             kbdHook = new KeyboardHook();
             kbdHook.KeyboardEvent += kbdHook_KeyboardEvent;
@@ -36,7 +37,7 @@ namespace dotSwitcher
             }
         }
 
-        void kbdHook_KeyboardEvent(object sender, KeyboardEventArgs e)
+        private void kbdHook_KeyboardEvent(object sender, KeyboardEventArgs e)
         {
             if (currentTextBox != null)
             {
@@ -59,6 +60,7 @@ namespace dotSwitcher
             shortcutTextBox.Leave += unsetCurrentInput;
             shortcutTextBox.GotFocus += setCurrentInput;
             shortcutTextBox.Enter += setCurrentInput;
+
             shortcutTextBox.Text = settings.SwitchHotkey.ToString();
 
 
@@ -75,20 +77,90 @@ namespace dotSwitcher
 
                 //comboBoxAdditionalSwitch.
             }
+<<<<<<< Updated upstream
+=======
+
+            var locales = LowLevelAdapter.GetkeyboardLayouts();
+            for (int i = 0; i < locales.Length; i++)
+            {
+                ComboBox comboBoxKeyboarLayouts = new ComboBox();
+                ComboBox comboBoxSwitchKey = new ComboBox();
+                GenerateCombox(locales[i].Lang, i, comboBoxKeyboarLayouts, comboBoxSwitchKey);
+                InicializeSwitchSettingsLine(comboBoxKeyboarLayouts, comboBoxSwitchKey);
+            }
         }
+
+        private string GetKeyCombinationString(Keys[] keys)
+        {
+            var strings = keys.Select(KeyName);
+            return string.Join(" + ", strings);
+        }
+
+        private string KeyName(Keys key)
+        {
+            return Enum.GetName(typeof(Keys), key);
+>>>>>>> Stashed changes
+        }
+
 
         private void InicializeSwitchSettingsLine(ComboBox boxKeyboarLayouts, ComboBox boxHotkeys)
         {
-            var list = LowLevelAdapter.GetkeyboardLayouts();
-            var alreadySetLocale = ComboBoxes.Select(t => t.SelectedItem).ToList();
-            foreach (var locale in list)
+            var locales = LowLevelAdapter.GetkeyboardLayouts();
+            var keys = new List<Keys> { Keys.LShiftKey, Keys.RShiftKey, Keys.LControlKey, Keys.RControlKey };
+
+            var usedItems = ComboBoxes.Select(t => t.SelectedItem).ToList();
+            foreach (var locale in locales)
             {
-                if(!alreadySetLocale.Contains(locale))
+                if (!usedItems.Contains(locale))
                     boxKeyboarLayouts.Items.Add(locale.Lang);
             }
-
+            foreach (var key in keys)
+            {
+                if (!usedItems.Contains(key))
+                    boxHotkeys.Items.Add(key);
+            }
         }
 
+
+        private bool GenerateCombox(string locale, int counter, ComboBox comboBoxKeyboarLayouts, ComboBox comboBoxSwitchKey)
+        {
+
+            const int SHIFT = 25;
+
+            try
+            {
+                //
+                // comboBoxSwitchKey
+                //
+                comboBoxSwitchKey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+                comboBoxSwitchKey.FormattingEnabled = true;
+                comboBoxSwitchKey.Location = new System.Drawing.Point(135, 115 + counter * SHIFT);
+                comboBoxSwitchKey.Name = "comboBoxSwitchKey" + locale;
+                comboBoxSwitchKey.Size = new System.Drawing.Size(121, 21);
+                comboBoxSwitchKey.TabIndex = 14;
+
+                //
+                // comboBoxKeyboarLayouts
+                //
+                comboBoxKeyboarLayouts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+                comboBoxKeyboarLayouts.FormattingEnabled = true;
+                comboBoxKeyboarLayouts.Location = new System.Drawing.Point(8, 115 + counter * SHIFT);
+                comboBoxKeyboarLayouts.Name = "comboBoxKeyboarLayouts" + locale;
+                comboBoxKeyboarLayouts.Size = new System.Drawing.Size(121, 21);
+                comboBoxKeyboarLayouts.TabIndex = 11;
+
+                this.switchKeyboardGroupbox.Controls.Add(comboBoxSwitchKey);
+                this.switchKeyboardGroupbox.Controls.Add(comboBoxKeyboarLayouts);
+                Size initialSize = this.switchKeyboardGroupbox.Size;
+                this.switchKeyboardGroupbox.Size = new System.Drawing.Size(initialSize.Width, initialSize.Height + SHIFT);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
