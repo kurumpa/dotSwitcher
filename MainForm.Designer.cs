338ef6804dd233d986cd47f89fe00d0a0dea5f64
BladeMight <blademight@gmail.com>
2016-06-21 14:38:53 +0300

    UI reposition

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -178,7 +178,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(169, 115);
+            this.label3.Location = new System.Drawing.Point(175, 115);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(149, 13);
             this.label3.TabIndex = 13;
